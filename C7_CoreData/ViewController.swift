05b04a982f3afcc2c465e03ca2adbe76cb665b17
Roy <b10715039@ntust.edu.tw>
2022-04-13 15:29:43 +0800

    Initial Commit

---
+++ C7_CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  C7_CoreData
+//
+//  Created by mac12 on 2022/4/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
